88479c7db2ed67855e09e5b231689d378756e5bd
AniketGunjal1 <Aniket.Gunjal@mindtree.com>
2022-08-18 10:56:42 +0530

    first commit

---
+++ src/Caculator/calculatorfunction.java
@@ -0,0 +1,28 @@
+package Caculator;
+
+public class calculatorfunction {
+
+	public static void main(String[] args) {
+		int a=10,b=20,c;
+		//Arithmetic operators
+		c=a+b;
+		System.out.println("Addition Is: "+c);
+		
+		c=b-a;
+		System.out.println("Substraction Is: "+c);
+		
+		c=b/a;
+		System.out.println("Division Is: "+c);
+		
+		c=a*b;
+		System.out.println("Multiplication Is: "+c);
+		
+		c=b%a;
+		System.out.println("Reminder Is: "+c);
+		
+
+	}
+
+	}
+
+
